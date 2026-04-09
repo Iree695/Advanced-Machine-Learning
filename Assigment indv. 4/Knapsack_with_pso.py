08349ee789fcde34d156696a90714221ddcca892
Irene <ivalleares@gmail.com>
2026-04-09 17:16:19 +0200

    class particle and other changes that i don't know why they appear

--- Assigment indv. 4/Knapsack_with_pso.py
+++ Assigment indv. 4/Knapsack_with_pso.py
@@ -31,3 +31,38 @@ def fitness(pose, weights, values, capacity):
 def sigmoid(v):
     func = 1 / 1 + np.exp(-np.clip(v, -15, 15))
     return func
+
+# Class Particle
+# - Position: binary vector of length N_items
+# - Speed: real vector
+# - Personal best: looks for the max
+
+class Particle:
+    def __init__(self, n_items):
+        # Position
+        position = []
+        for _ in range(n_items):
+            position.append(random.randint(0, 1))
+        self.position = np.array(position, dtype = float)
+
+        # Speed
+        speed = []
+        for _ in range(n_items):
+            speed.append(random.uniform(-1, 1))
+        self.speed = np.array(speed) 
+
+        # Personal Best --> Max means that it starts from -inf
+        self.personal_best_position = self.position.copy()
+        self.personal_best_value = float("-inf")
+
+    def evaluate(self, weight, values, capacity):
+        """With this function we can evaluate the current position and  update the personal best"""
+        # For evaluating the current position
+        evalue = fitness(self.position, self.weight, self.values, self.capacity)
+
+        # For pdating the personal best
+        if evalue > self.personal_best_value:
+            self.personal_best_value = evalue
+            self.personal_best_position = self.position.copy()
+        else:
+            return evalue
