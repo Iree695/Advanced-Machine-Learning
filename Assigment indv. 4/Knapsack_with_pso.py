0f3e26d2925d23779cbcf464e4dd86e403cfbe80
Irene <ivalleares@gmail.com>
2026-04-09 16:56:04 +0200

    fitness function

--- Assigment indv. 4/Knapsack_with_pso.py
+++ Assigment indv. 4/Knapsack_with_pso.py
@@ -9,4 +9,14 @@ def generate_items(n_items=10):
     values = np.round(np.random.uniform(10, 100, size = n_items), 0)
     return weights, values
 
+# Fitness function
+# - If we exceed the weight → fitness = 0 so instead of minimazing the solution we maximize it.
 
+def fitness(pose, weights, values, capacity):
+    total_weight = np.dot(pose, weights)
+    total_values = np.dot(pose, values)
+
+    if total_weight > capacity:
+        return 0
+    else:
+        return total_values
