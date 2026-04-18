b0dc33053d333fac502f621d133965df21cf8ef6
Irene <ivalleares@gmail.com>
2026-04-18 13:14:50 +0200

    def init terminar y def run

--- Assigment 4/tsp_aco.py
+++ Assigment 4/tsp_aco.py
@@ -13,3 +13,37 @@ class ACO:
         self.beta = beta # --> distance importance
         self.rho = rho # --> evaporation rate
         self.q = q # --> pheromone deposit factor
+
+        # Initialize pheromones
+        self.pheromone = np.ones((self.n_cities, self.n_cities)) / self.n_cities
+
+        # Compute visibility ( inverse of distance )
+        self.visibility = 1/ (dist_matrix + np.eye(self.n_cities) * 1e-10)
+        np.fill_diagonal(self.visibility, 0)
+
+    def run(self):
+        best_tour = None
+        best_length = float("inf")
+        history = []
+
+        for iteration in range(self.n_iterations):
+            tours = self.generate_tours()
+            lenghts = []
+            for tour in tours:
+                lenght = self.tour_lenght(tour)
+                lenghts.append(lenght)
+
+                min_idx = np.argmin(lenghts)
+                if lenghts[min_idx] < best_length:
+                    best_length = lenghts[min_idx]
+                    best_tour = tours[min_idx]
+                
+                history.append(best_length)
+                self.update_pheromones(tours, lenghts)
+
+                if iteration % 10 == 0:
+                    print(f"Iteration {iteration}, Best Length: {best_length}")
+
+            return best_tour, best_length, history
+        
+            
